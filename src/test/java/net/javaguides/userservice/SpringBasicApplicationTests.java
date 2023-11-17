9ecc97c4efd810413a476b1c34ff5967ec379e54
XxJulioCarvalhoxX <juliocesar_porto25@hotmail.com>
2023-11-17 20:33:48 -0300

    inicial commit

---
+++ src/test/java/net/javaguides/userservice/SpringBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
